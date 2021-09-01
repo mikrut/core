0d638f9671c15f43c3aa7af30b61ea36669ea1b1
mikrut <m.krutyakov@yandex.ru>
2021-09-01 22:01:19 +0000

    Increase rs485 master reliability

--- homeassistant/components/water_meter_rs485/rs485/master.py
+++ homeassistant/components/water_meter_rs485/rs485/master.py
@@ -21,7 +21,10 @@ from homeassistant.components.water_meter_rs485.const import (
 from .protocol import ReadVolumeRequest, ReadVolumeResponse
 
 
-class Master:
+class Master(object):
+    WRITE_TIMEOUT = 1
+    READ_TIMEOUT = 1
+
     def __init__(self, config: Mapping[str, Any]):
         self._serial_port = serial.Serial(
             port=None,
@@ -29,28 +32,33 @@ class Master:
             bytesize=ByteSizeType.get_by_description(config[CONF_BYTE_SIZE]),
             parity=ParityType.get_by_description(config[CONF_PARITY]),
             stopbits=StopBitsType.get_by_description(config[CONF_STOP_BITS]),
+            write_timeout=Master.WRITE_TIMEOUT,
+            timeout=Master.READ_TIMEOUT,
         )
         self._serial_port.port = config[CONF_SERIAL_PORT]
-        self._serial_port.timeout = 2
         self._lock = threading.Lock()
 
     def open(self):
-        self._serial_port.open()
+        with self._lock:
+            self._serial_port.open()
 
     def read_value(self, unique_id: str) -> Optional[Decimal]:
         with self._lock:
+            self._serial_port.reset_input_buffer()
+
             sensor_id = int(unique_id)
 
-            read_request = ReadVolumeRequest(address=sensor_id)
-            self._serial_port.write(read_request.frame)
+            read_frame = ReadVolumeRequest(address=sensor_id).frame
+            bytes_written = self._serial_port.write(read_frame)
+            assert bytes_written == read_frame.length
 
             response_frame = self._serial_port.read(
                 ReadVolumeResponse.EXPECTED_TOTAL_LENGTH
             )
-            if len(response_frame) < ReadVolumeResponse.EXPECTED_TOTAL_LENGTH:
+            if len(response_frame) != ReadVolumeResponse.EXPECTED_TOTAL_LENGTH:
                 return None
 
             response = ReadVolumeResponse(response_frame)
             assert response.address == sensor_id
-            assert response.id == read_request.id
+            assert response.id == read_frame.id
             return response.volume
